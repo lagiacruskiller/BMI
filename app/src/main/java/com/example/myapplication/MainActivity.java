8cbee2a83331815b69c7d2a75829db27a3ef0e4e
miia <miia@com.tw>
2019-10-08 15:44:06 +0800

    Updated code: Renamed used tools in the code to fit their usage Added degree of BMI(using if-else clause to identify which degree the patient is currently at) Renamed all id of used buttons, edittexts, and textviews (to identify what their usages) Changed Int to Float Removed redundant types on findViewById Changed word from "over light" to "under weight"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,37 +10,37 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button01;
-    private EditText editText01, editText02;
-    private TextView textView03, textView04;
+    private Button calcbutton;
+    private EditText heightinput, weightinput;
+    private TextView displaybmi, conditionid;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button01 = (Button) findViewById(R.id.calcbutton);
-        editText01 = (EditText) findViewById(R.id.heightinput);
-        editText02 = (EditText) findViewById(R.id.weightinput);
-        textView03 = (TextView) findViewById(R.id.displaybmi);
-        textView04 = (TextView) findViewById(R.id.conditionid);
+        calcbutton = findViewById(R.id.calcbutton);
+        heightinput = findViewById(R.id.heightinput);
+        weightinput = findViewById(R.id.weightinput);
+        displaybmi = findViewById(R.id.displaybmi);
+        conditionid = findViewById(R.id.conditionid);
 
-        button01.setOnClickListener(new Button.OnClickListener(){
+        calcbutton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v){
-                int height = Integer.parseInt(editText01.getEditableText().toString());
-                float height01 = (float) height / 100;
-                int weight = Integer.parseInt(editText02.getEditableText().toString());
-                float BMI = (float) weight / (height01 * height01);
-                textView03.setText("BMI = " + BMI);
-                if(BMI< 15) textView04.setText("Extremely over light");
-                else if (BMI< 16) textView04.setText("Over light");
-                else if (BMI< 18.5) textView04.setText("Slighty light");
-                else if (BMI< 25) textView04.setText("Healthy");
-                else if (BMI< 30) textView04.setText("Over Weight");
-                else if (BMI< 35) textView04.setText("Obesity Level I");
-                else if (BMI< 40) textView04.setText("Obesity Level II");
-                else textView04.setText("Obesity Level III");
+                float height = Float.parseFloat(heightinput.getEditableText().toString());
+                float height01 = height / 100;
+                float weight = Float.parseFloat(weightinput.getEditableText().toString());
+                float BMI = weight / (height01 * height01);
+                displaybmi.setText("BMI = " + BMI);
+                if(BMI< 15) conditionid.setText("Extremely under weight");
+                else if (BMI< 16) conditionid.setText("Under weight");
+                else if (BMI< 18.5) conditionid.setText("Slighty light");
+                else if (BMI< 25) conditionid.setText("Healthy");
+                else if (BMI< 30) conditionid.setText("Over Weight");
+                else if (BMI< 35) conditionid.setText("Obesity Level I");
+                else if (BMI< 40) conditionid.setText("Obesity Level II");
+                else conditionid.setText("Obesity Level III");
             }
         });
     }
