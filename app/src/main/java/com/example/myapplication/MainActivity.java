2a5cf64471599e1510f21a8cc0b12f85246288c3
miia <miia@com.tw>
2019-10-08 14:51:49 +0800

    Updated code: Added degree of BMI(using if-else clause to identify which degree the patient is currently at) Renamed all id of used buttons, edittexts, and textviews (to identify what their usages)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,22 +8,22 @@ import android.view.View;
 
 import android.os.Bundle;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     private Button button01;
     private EditText editText01, editText02;
-    private TextView textView03;
+    private TextView textView03, textView04;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button01 = (Button) findViewById(R.id.button);
-        editText01 = (EditText) findViewById(R.id.editText);
-        editText02 = (EditText) findViewById(R.id.editText2);
-        textView03 = (TextView) findViewById(R.id.textView3);
+        button01 = (Button) findViewById(R.id.calcbutton);
+        editText01 = (EditText) findViewById(R.id.heightinput);
+        editText02 = (EditText) findViewById(R.id.weightinput);
+        textView03 = (TextView) findViewById(R.id.displaybmi);
+        textView04 = (TextView) findViewById(R.id.conditionid);
 
         button01.setOnClickListener(new Button.OnClickListener(){
             @Override
@@ -33,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
                 int weight = Integer.parseInt(editText02.getEditableText().toString());
                 float BMI = (float) weight / (height01 * height01);
                 textView03.setText("BMI = " + BMI);
+                if(BMI< 15) textView04.setText("Extremely over light");
+                else if (BMI< 16) textView04.setText("Over light");
+                else if (BMI< 18.5) textView04.setText("Slighty light");
+                else if (BMI< 25) textView04.setText("Healthy");
+                else if (BMI< 30) textView04.setText("Over Weight");
+                else if (BMI< 35) textView04.setText("Obesity Level I");
+                else if (BMI< 40) textView04.setText("Obesity Level II");
+                else textView04.setText("Obesity Level III");
             }
         });
     }
